a4fd64366b0048206e3580a4501b9a2861093dfb
DanilaViatkin <danilapots@mail.ru>
2023-09-01 11:55:08 +0300

    Add location controller

---
+++ src/main/java/by/refor/mobilefarm/controller/LocationController.java
@@ -0,0 +1,24 @@
+package by.refor.mobilefarm.controller;
+
+import by.refor.mobilefarm.model.bo.Location;
+import by.refor.mobilefarm.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/locations")
+public class LocationController {
+    private final LocationService locationService;
+
+    @Autowired
+    public LocationController(LocationService locationService){
+        this.locationService = locationService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Location createLocation(@RequestBody Location location){
+        return locationService.createLocation(location);
+    }
+}
