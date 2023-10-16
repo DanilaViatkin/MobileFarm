ce2dc6d5f9cff7da4622d61cb5c045184e1b9829
Анна <annamiha@list.ru>
2023-10-16 17:35:39 +0300

    add @jsonidentityinfo

--- src/main/java/by/refor/mobilefarm/model/entity/FeedGroupEntity.java
+++ src/main/java/by/refor/mobilefarm/model/entity/FeedGroupEntity.java
@@ -1,5 +1,7 @@
 package by.refor.mobilefarm.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -12,6 +14,7 @@ import java.util.Objects;
 @Table(name = "feed_groups")
 @Data
 @Accessors(chain = true)
+@JsonIdentityInfo(generator= ObjectIdGenerators.PropertyGenerator.class, property="id")
 public class FeedGroupEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
